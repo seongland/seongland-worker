41cbaed61dd0292126ae19606c254e90ef30494d
seonglae <sungle3737@gmail.com>
2021-09-05 21:48:47 +0900

    cd: accountland branch

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /* Step 1: Domain name */
-const MY_DOMAIN = 'next.seongland.com'
-const ANOTHER = 'seongland-seonglae.cloud.okteto.net'
+const MY_DOMAIN = 'account.seongland.com'
+const ANOTHER = 'accountland-seonglae.cloud.okteto.net'
 
 /* CONFIGURATION ENDS HERE */
-addEventListener('fetch', (event) => {
+addEventListener('fetch', event => {
   const fetchEvent = event as FetchEvent
   fetchEvent.respondWith(fetchAndApply(fetchEvent.request))
 })
@@ -28,9 +28,7 @@ function handleOptions(request: Request) {
   // Handle standard OPTIONS request.
   else
     return new Response(null, {
-      headers: {
-        Allow: 'GET, HEAD, POST, PUT, OPTIONS',
-      },
+      headers: { Allow: 'GET, HEAD, POST, PUT, OPTIONS' },
     })
 }
 
@@ -41,52 +39,20 @@ async function fetchAndApply(request: Request) {
   url.hostname = ANOTHER
 
   let response
-  if (url.pathname.endsWith('js')) {
-    response = await fetch(url.toString())
-    let body = await response.text()
-    response = new Response(
-      body.replace(String.raw`/${ANOTHER}/g`, MY_DOMAIN),
-      response,
-    )
-    response.headers.set('Content-Type', 'application/x-javascript')
-    return response
-  } else if (
-    url.pathname.startsWith('/api') &&
-    !url.pathname.endsWith('robots.txt') &&
-    !url.pathname.endsWith('sitemap.xml')
-  ) {
-    response = await fetch(url.toString(), {
-      body: request.body,
-      headers: {
-        'content-type': 'application/json;charset=UTF-8',
-        'user-agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
-      },
-      method: 'POST',
-    })
-    response = new Response(response.body, response)
-    response.headers.set('Access-Control-Allow-Origin', '*')
-    return response
-  } else {
-    response = await fetch(url.toString(), {
-      body: request.body,
-      headers: request.headers,
-      method: request.method,
-    })
-    response = new Response(response.body, response)
-    response.headers.delete('Content-Security-Policy')
-    response.headers.delete('X-Content-Security-Policy')
-  }
-
+  response = await fetch(url.toString(), {
+    body: request.body,
+    headers: request.headers,
+    method: request.method,
+  })
+  response = new Response(response.body, response)
+  response.headers.delete('Content-Security-Policy')
+  response.headers.delete('X-Content-Security-Policy')
   return appendJavascript(response)
 }
 
 class MetaRewriter {
   element(element: HTMLElement) {
-    if (
-      element.getAttribute('property') === 'og:url' ||
-      element.getAttribute('name') === 'twitter:url'
-    )
+    if (element.getAttribute('property') === 'og:url' || element.getAttribute('name') === 'twitter:url')
       element.setAttribute('content', MY_DOMAIN)
     if (element.getAttribute('name') === 'apple-itunes-app') element.remove()
   }
@@ -98,7 +64,7 @@ class HeadRewriter {
 
 class BodyRewriter {
   constructor() {}
-  element(element: HTMLElement) {}
+  element() {}
 }
 
 async function appendJavascript(res: Response) {
